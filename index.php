0918a5c2cad832b9e11fb5f52fa1597a5654ebb7
YohanDies <yohandies97@gmail.com>
2017-11-09 09:42:00 +0100

    structure HTML:un message explicite

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GIT</title>
+    <link rel="stylesheet" type="text/css" href="ui.css">
+</head>
+<body>
+    <h1>GIT</h1>
+    <div class="container">
+        <div class="row">
+            <img src="poke.png" alt="">
+
+        </div>
+    </div>
+
+
+    
+</body>
+</html>
